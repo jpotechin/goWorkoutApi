7b0c2846e9ac883c72530efdb69430abef5a7846
Joseph Potechin <45020840+jpotechin@users.noreply.github.com>
2025-07-19 19:06:43 -0400

    Added Chi Router

--- main.go
+++ main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/jpotechin/goWorkoutApi/internal/app"
+	"github.com/jpotechin/goWorkoutApi/internal/routes"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
+	app, err := app.NewApplication()
+
+	if err != nil {
+		panic(err)
+	}
+
+	r := routes.SetupRoutes(app)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	app.Logger.Printf("We are running on port %d", port)
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal()
+	}
+
+}
