7b2b3bc786ee2d9b785290b5d151ad77a7acb59c
Joseph Potechin <45020840+jpotechin@users.noreply.github.com>
2025-07-19 20:29:48 -0400

    Added basic Postgres DB connection

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,30 +1,38 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/jpotechin/goWorkoutApi/internal/api"
+	"github.com/jpotechin/goWorkoutApi/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// stores will go here
+	pgDB, err := store.Open()
 
+	if err != nil {
+		return nil, err
+	}
 	// out handlers will go here
 	workoutHandler := api.NewWorkoutHandler()
 
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
