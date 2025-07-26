036ad9b471c0a08742d0f7116f37498a2c7d9610
jpotechin <45020840+jpotechin@users.noreply.github.com>
2025-07-26 16:43:13 -0400

    Middleware (#6)
    * Get User Token
    
    * Added middleware and updated CRUD

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/jpotechin/goWorkoutApi/internal/api"
+	"github.com/jpotechin/goWorkoutApi/internal/middleware"
 	"github.com/jpotechin/goWorkoutApi/internal/store"
 	"github.com/jpotechin/goWorkoutApi/migrations"
 )
@@ -17,6 +18,7 @@ type Application struct {
 	WorkoutHandler *api.WorkoutHandler
 	UserHandler    *api.UserHandler
 	TokenHandler   *api.TokenHandler
+	Middleware     middleware.UserMiddleware
 	DB             *sql.DB
 }
 
@@ -43,12 +45,14 @@ func NewApplication() (*Application, error) {
 	workoutHandler := api.NewWorkoutHandler(workoutStore, logger)
 	userHandler := api.NewUserHandler(userStore, logger)
 	tokenHandler := api.NewTokenHandler(tokenStore, userStore, logger)
+	middlewareHandler := middleware.UserMiddleware{UserStore: userStore}
 
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
 		UserHandler:    userHandler,
 		TokenHandler:   tokenHandler,
+		Middleware:     middlewareHandler,
 		DB:             pgDB,
 	}
 
